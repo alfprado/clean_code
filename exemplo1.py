5d67aeb8b105ec8fc6c34cc266759b4ddac5293a
André Prado <prado_fernandes@yahoo.com.br>
2020-06-06 22:32:34 -0300

    Adiciona exemplo 1

---
+++ exemplo1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def elapse(year):
+    days = 365
+    '''
+    Substituir esse trecho pela função is_leap
+
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    '''
+    if is_leap(year):
+        days += 1
+    for day in range(1, days + 1):
+        print("Day {} of {}".format(day, year))
+
+def is_leap(year):
+    return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
+
+if __name__ == "__main__":
+    elapse(2000)
+    
